ccdfd5760bf693c609bd87cbbbc66ca2b6cc7147
pizzi9523 <pizzitolagiuseppe@live.it>
2022-12-03 14:30:18 +0100

    add media-query navbar mobile

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log('ciao');
+
+const hamburgerButton = document.querySelector(".hamburger-menu-button");
+
+hamburgerButton.addEventListener('click', showMenu());
+
+function showMenu() {
+console.log(hamburgerButton);
+}
+
