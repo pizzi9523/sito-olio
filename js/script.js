b50d6673bbc0516bb826ece263e6bd1fefe893a5
pizzi9523 <pizzitolagiuseppe@live.it>
2022-12-04 19:31:25 +0100

    complete forth_section and fix colours

--- js/script.js
+++ js/script.js
@@ -1,29 +1,44 @@
 const hamburgerButtonOpen = document.querySelector(".hamburger-menu-button-open");
 const hamburgerButtonClose = document.querySelector(".hamburger-menu-button-close");
+const orderButton = document.querySelector(".order_button");
 
 const menuMobile = document.querySelector(".menu-mobile");
 
 hamburgerButtonOpen.addEventListener('click', showMenu);
 hamburgerButtonClose.addEventListener('click', hideMenu);
+orderButton.addEventListener('mousedown', clickButton);
+orderButton.addEventListener('mouseup', leaveButton);
 
 
 
 
 function showMenu() {
-    console.log(menuMobile);
+    // console.log(menuMobile);
     menuMobile.style.height = '100vh';
     hamburgerButtonOpen.style.display = 'none';
     hamburgerButtonClose.style.display = 'block';
-    console.log('click');
+    // console.log('click');
 
 }
 
 function hideMenu() {
-    console.log(menuMobile);
+    // console.log(menuMobile);
     menuMobile.style.height = '0';
     hamburgerButtonOpen.style.display = 'block';
     hamburgerButtonClose.style.display = 'none';
-    console.log('click');
+    // console.log('click');
+}
+
+function clickButton() {
+    orderButton.style.filter = 'drop-shadow(0px 0px 7px white)';
+    orderButton.style.color = 'white';
+    orderButton.style.borderColor = 'white';
+}
+
+function leaveButton() {
+    orderButton.style.filter = 'drop-shadow(0px 0px 7px black)'
+    orderButton.style.color = 'black';
+    orderButton.style.borderColor = 'black';
 }
 
 
